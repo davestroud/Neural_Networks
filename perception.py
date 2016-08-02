fe9e20594021f8e67006ba936a71f53d43969f34
davestroud <david@davidstroud.me>
2016-08-02 15:30:37 -0500

    activate threshold function in perceptron file

--- perception.py
+++ perception.py
@@ -6,28 +6,22 @@
 #   Finish writing the activate() method by using numpy.dot and adding in the thresholded
 #   activation function
 
-import numpy
+import numpy as np
 
 class Perceptron:
-
-    
     def activate(self,inputs):
         '''Takes in @param inputs, a list of numbers.
         @return the output of a threshold perceptron with
         given weights, threshold, and inputs.
-        ''' 
-               
-        
-        #YOUR CODE HERE
-
-        #TODO: calculate the strength with which the perceptron fires
-
-        #TODO: return 0 or 1 based on the threshold
-            
+        '''        
+        y = np.dot(self.weights, inputs)
+        if y >= self.threshold:
+            result = 1
+        else:
+            result = 0
         return result
 
-        
-        
+           
     def __init__(self,weights=None,threshold=None):
         if weights is not None:
             self.weights = weights
